d4aabc8a59d2391c20110e7231aee2b887139b30
Mudit Bisht <muditsinghbisht@gmail.com>
2024-08-21 18:00:06 +0530

    add session functionality

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const fs = require('fs');
 const cookieParser = require('cookie-parser');
+const uuid = require('uuid');
+const masterPassword = process.env["MASTER_PASSWORD"];
 
-const PASSWORD = "password";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use((req, res, next) => {
+  let authenticated = false;
+
+  const { session } = req.cookies;
+  const sessionTokenList = JSON.parse(fs.readFileSync("./data/sessions.json"));
+
+  if (sessionTokenList.some((a) => a == session)) {
+    authenticated = true;
+  }
+
+  req.authenticated = authenticated;
+
+  next();
+});
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  console.log("Cookies:", req.cookies);
-
   const jsonBlog = fs.readFileSync("./data/blogs.json");
   const blogs = JSON.parse(jsonBlog);
-
-  res.appendHeader("Set-Cookie", "user=user-1");
-  res.render("index", {blogs});
+  res.render("index", { blogs, authenticated: req.authenticated });
 });
 
 app.get("/create-blog", (req, res) => {
-  res.render("create-blog");
+
+  if(req.authenticated) {
+    res.render("create-blog");
+  } else {
+    res.send('<h1>Not Authenticated</h1><a href="/">Home</a>');
+  }
 });
 
 app.post("/create-blog", (req, res) => {
@@ -65,6 +81,34 @@ app.get("/blog/:slug", (req, res) => {
   res.render('blog', { blog });
 });
 
+
+app.get("/login", (req, res) => {
+  res.render('login');
+});
+
+app.post("/login", (req, res) => {
+  const { password } = req.body;
+  if (password == masterPassword) {
+    const sessionToken = uuid.v4();
+    res.cookie("session", sessionToken);
+    const sessionTokenList = JSON.parse(fs.readFileSync("./data/sessions.json"));
+    sessionTokenList.push(sessionToken);
+    fs.writeFileSync("./data/sessions.json", JSON.stringify(sessionTokenList));
+    res.redirect('/');
+  }
+});
+
+app.get('/logout', (req, res) => {
+  const { session } = req.cookies;
+
+  // remove from session JSON list
+  let sessionTokenList = JSON.parse(fs.readFileSync("./data/sessions.json"));
+  sessionTokenList = sessionTokenList.filter(s => s != session);
+  fs.writeFileSync("./data/sessions.json", JSON.stringify(sessionTokenList));
+
+  res.clearCookie('session');
+  res.render('logout')
+});
 
 app.listen(3000, () => {
   console.log("Server running at 3000!");
